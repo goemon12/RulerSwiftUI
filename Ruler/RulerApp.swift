99334adbf501c9b15ec794aa372e813afd13c4eb
Tadahiro Kato <gomeon12.tc4@gmail.com>
2021-07-05 06:45:57 +0900

    Initial Commit

---
+++ Ruler/RulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RulerApp.swift
+//  Ruler
+//
+//  Created by goemon12 on 2021/07/05.
+//
+
+import SwiftUI
+
+@main
+struct RulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
